d3aba4ab3227996d9d7e9e21716074abb74d741b
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-01 10:48:38 +0000

    Fri, Sep 1, 2023, 12:48 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -178,7 +178,7 @@ int find_min(void)
     int min = voter_count;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes < min && candidates[i].votes < min)
+        if (candidates[i].votes < min && candidates[i].eliminated == false)
         {
             min = candidates[i].votes;
         }
