04a0586be712bb7aff991d5d13fe8b32f4157a64
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-01 19:43:09 +0000

    Fri, Sep 1, 2023, 9:43 PM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,27 +1,26 @@
 #include <cs50.h>
 #include <stdio.h>
 
-
 int main(void)
 {
-   int height;
-   do
-   {
-    height = get_int("Enter height: ");
-   }
-   while (height < 1 || height > 8);
+    int height;
+    do
+    {
+        height = get_int("Enter height: ");
+    }
+    while (height < 1 || height > 8);
 
-   for (int row = 0; row < height; row++)
-   {
-    for (int space = 0; space < height - row -1; space++)
+    for (int row = 0; row < height; row++)
     {
-        printf(" ");
+        for (int space = 0; space < height - row -1; space++)
+        {
+            printf(" ");
+        }
+        for (int col = 0; col < row; col++)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
-    for (int col = 0; col < row; col++)
-    {
-        printf("#");
-    }
-    printf("\n");
-   }
-   return true;
+    return true;
 }
