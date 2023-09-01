ae502500f4237cd154705365f70ce4347137f3f6
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-01 11:19:35 +0000

    Fri, Sep 1, 2023, 1:19 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -214,7 +214,7 @@ void eliminate(int min)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (!candidates[i].votes == min)
+        if (candidates[i].votes == min)
         {
             candidates[i].eliminated = true;
         }
