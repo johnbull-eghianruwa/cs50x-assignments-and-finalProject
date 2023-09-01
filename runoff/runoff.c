fef7edd401df0bd97b7ed85e5a2803f3335b1b0c
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-01 07:33:37 +0000

    Fri, Sep 1, 2023, 9:33 AM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -127,7 +127,11 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    // TODO
+    // name is a match for the name of a valid candidate
+    for (int i = 0; i < candidate_count; i++)
+    {
+        
+    }
     return false;
 }
 
