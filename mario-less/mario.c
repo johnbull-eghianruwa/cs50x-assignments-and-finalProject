acf7bb056e78807f2c650c3acc1cfa3b84e7d629
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-01 21:18:19 +0000

    Fri, Sep 1, 2023, 11:18 PM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -9,5 +9,14 @@ int main(void)
     n = get_int("Height: ");
   }
   while (height < 1 || height > 8)
-  
+
+  for (int i = 0; < height; i++)
+  {
+    for (int j = 0; j < i; j++)
+    {
+        printf("#");
+    }
+    printf("\n");
+  }
+
 }
