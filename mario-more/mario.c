12cc035189716b68507b509d1b2e7c322d1ca814
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-01 21:55:42 +0000

    Fri, Sep 1, 2023, 11:55 PM +02:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,11 +3,25 @@
 
 int main(void)
 {
-  int n;
-  do
-  {
-    n = get_int("Height: ");
-  }
-  while (n < 1 || height > 8)
-  // printf
+ // GET INPUT OF THE HEIGHT
+    int height;
+    do
+    {
+        height = get_int("height: ");
+    }
+    while (height < 1 || height > 8);
+
+    // PRINT DESIRED PYRAMID HEIGHT
+    for (int i = 0; i < height; i++)
+    {
+        // SET PERIMETERS FOR THE COLUMNS TO RIGHT
+        for (int j = 0; j < height+i+3; j++)
+        {
+            if (j == height || j == height+1 || i +j < height-1)
+                printf(" ");
+            else
+                 printf("#");
+        }
+        printf("\n");
+    }
 }
