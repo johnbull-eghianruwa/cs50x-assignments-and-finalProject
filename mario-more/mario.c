35fb26cf6545d21842143687ca15a028902c20f1
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-01 19:52:18 +0000

    Fri, Sep 1, 2023, 9:52 PM +02:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,31 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int height;
+    do
+    {
+        height = get_int("Enter height: ");
+    }
+    while (height < 1 || height > 8);
+
+    for (int row = 0; row < height; row++)
+    {
+        for (int space = 0; space < height - row -1; space++)
+        {
+            printf(" ");
+        }
+        for (int col = 0; col < row; col++)
+        {
+            printf("#");
+        }
+        for (int col = 0; < row; col++)
+        {
+            printf("  ")
+        }
+        printf("\n");
+    }
+    return true;
+
 }
