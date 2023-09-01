10efedc8d7cfb372d4a023f728e1d0ae6f660ff3
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-01 22:09:40 +0000

    Sat, Sep 2, 2023, 12:09 AM +02:00

--- cash/cash.c
+++ cash/cash.c
@@ -37,30 +37,55 @@ int main(void)
 
 int get_cents(void)
 {
-    // TODO
-    return 0;
+    int cents;
+    do
+    {
+        cents = get_int("Cents owned: ");
+    }
+    while (cents < 0);
+    return cents;
 }
 
 int calculate_quarters(int cents)
 {
-    // TODO
-    return 0;
+    int quarters = 0;
+    while (cents >= 24)
+    {
+        cents = cents -24;
+        quarters++;
+    }
+    return quarters;
 }
 
 int calculate_dimes(int cents)
 {
-    // TODO
-    return 0;
+    int dimes = 0;
+    while (cents >= 11)
+    {
+        cents = cents - 11;
+        dimes++;
+    }
+    return dimes;
 }
 
 int calculate_nickels(int cents)
 {
-    // TODO
-    return 0;
+    int nickels = 0;
+    while (cents >= 5)
+    {
+        cents = cents -5;
+        nickels++;
+    }
+    return nickels;
 }
 
 int calculate_pennies(int cents)
 {
-    // TODO
-    return 0;
+    int pennies = 0;
+    while (cents >= 1)
+    {
+        cents = cents -1;
+        pennies++;
+    }
+    return pennies;
 }
