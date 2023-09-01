0e569f0a9121bbc72d366708494f14a24235ee96
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-01 21:44:01 +0000

    Fri, Sep 1, 2023, 11:44 PM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -10,10 +10,10 @@ int main(void)
   }
   while (n < 1 || n > 8);
   for (row = 0; row < n; row++)
-  for (space = 0; space < n - row - 1; space++)
-  {
-    printf(" ");
-  }
+    for (space = 0; space < n - row -1; space++)
+    {
+        printf(" ");
+    }
   {
     for (col = 0; col <= row; col++)
     {
