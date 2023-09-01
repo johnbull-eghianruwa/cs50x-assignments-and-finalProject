e6046d16fa3853a4f93edf6ad83a89ea76cb0cd0
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-01 19:06:22 +0000

    Fri, Sep 1, 2023, 9:06 PM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,25 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+
+int main(void)
 {
-    
+    int height, row, column;
+
+    do
+    {
+        height = get_int("Enter height: ");
+    }
+
+    while (height < 1 || height > 8);
+
+    for (row = 0; row < height; row++)
+    {
+        for (column = 0; column < row; column++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    return 0;
 }
