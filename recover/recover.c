0b3bb31a83a8ab952e5366220295668738e8dca1
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-02 13:46:21 +0000

    Sat, Sep 2, 2023, 3:46 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -39,7 +39,10 @@ int main(int argc, char *argv[])
     {
         // Check if bytes indicate start of JPEG
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            
+        }
     }
 
-        
+
 }
