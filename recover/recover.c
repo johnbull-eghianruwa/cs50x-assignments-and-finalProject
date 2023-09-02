f08c3c3c8740618678490f4e8795df534274f836
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-02 13:25:34 +0000

    Sat, Sep 2, 2023, 3:25 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -38,6 +38,6 @@ int main(int argc, char *argv[])
     while (fread(buffer, sizeof(char), 512, input_file))
     {
         // Check if bytes indicate start of JPEG
-        if (buffer[0] == oxff && buffer[1] == oxd8)
+        if (buffer[0] == oxff && buffer[1] == oxd8 && buffer[2] == oxff )
     }
 }
