7856b0bf64e6e61d205846b4edad3652da1c6fa6
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-02 18:53:15 +0000

    Sat, Sep 2, 2023, 8:53 PM +02:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -10,6 +10,30 @@ int main(void)
 {
     // Prompt user for input
     string message = get_string("Enter message here: ");
+
+    // Iterate through each character of the string
+    for (int i = 0; = number = strlen(message); i < number; i++)
+    {
+        int binary[] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+        //Convert the character into ASCII
+        int decimal = message [i];
+
+        // Covert the ASCII into binary
+        int j = 0;
+        while (decimal > 0)
+        {
+            binary[j] = decimal % 2;
+            decimal = decimal / 2;
+            j++;
+        }
+        // Print the binary in reverse
+        for (int k = BITS_IN_BYTE - 1; k >= 0; k--)
+        {
+            print_bulb(binary[k]);
+        }
+        printf("\n");
+    }
 }
 
 void print_bulb(int bit)
