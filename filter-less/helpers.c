8672367ff23886196b16ad51733bad1722a81b85
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-02 09:15:16 +0000

    Sat, Sep 2, 2023, 11:15 AM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -4,6 +4,21 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Comb through each row
+    for (int i = 0; i < height; i++)
+    {
+        // Comb through each column
+        for (int j = 0; j < width; j++)
+        {
+            // Convert pixels to float
+            float Red = image[i][j].rgbtRed;
+            float Green = image[i][j].rgbtGreen;
+            float Blue = image[i][j].rgbtBlue;
+
+            // Find the average value
+            int average = round((Red + Green + Blue) / 3)
+        }
+    }
     return;
 }
 
