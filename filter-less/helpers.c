1640eeb6a784ed27eeeb4cb9cc9b70abd2f9f403
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-02 11:01:10 +0000

    Sat, Sep 2, 2023, 1:01 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -39,7 +39,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 
             // Find the updated pixle value
             int sepiaRed = round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
-            int sepiaGreen = round(0.349 * originalRed + 0.    686 * originalGreen + 0.168 * originalBlue);
+            int sepiaGreen = round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
             int sepiaBlue = round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
 
             // Update the pixel value if sepiaRed; sepiaGreen or sepiaBlue exceeds 255
