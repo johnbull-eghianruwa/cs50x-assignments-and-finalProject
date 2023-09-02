18109f559962605dfcb3d01762142d7ac04971d9
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-02 13:42:44 +0000

    Sat, Sep 2, 2023, 3:42 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -40,4 +40,13 @@ int main(int argc, char *argv[])
         // Check if bytes indicate start of JPEG
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
     }
+
+        // Write the JPEG filenames
+        sprintf(filename, "%03i.jpg", count_image);
+
+        // Open output_file for writing
+        output_file = fopen(filename, "w");
+
+        // Count number of images found
+        count_image += 1;
 }
