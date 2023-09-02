aee49d0a7eea9dc4de9a671d7cf1312c37389bef
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-02 13:04:16 +0000

    Sat, Sep 2, 2023, 3:04 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    // Check that the argument count is 2
+    if (argc != 2)
+    {
+        printf("Usage: ./recover IMAGE\n");
+        return 1;
+    }
+
+    // Open file for reading
+    FILE *input_file = fopen(argv[1], "r");
+
+    // Check that the input_file is valid
+    if (input_file == NULL)
+    {
+        printf("Could not  open file");
+    }
 }
