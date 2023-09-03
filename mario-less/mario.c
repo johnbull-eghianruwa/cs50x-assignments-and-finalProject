2effbd4b6d74b44b48b42f3fd926f8421f7aa404
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-03 18:48:30 +0000

    Sun, Sep 3, 2023, 8:48 PM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,18 +3,28 @@
 
 int main(void)
 {
+    // These variables represent the pyramid's height, row, column, and number of spaces respectively
     int n, row, col, space;
+
+    // The following code check input value is from 1 and 8
+    // If outside specify range the user is prompt again
     do
     {
+        // Prompt user for input
         n = get_int("Height: ");
     }
     while (n < 1 || n > 8);
+
+    // Print the pyramid base on specify height
     for (row = 0; row < n; row++)
     {
+        // Print number of spaces on row base on the width
         for (space = 0; space < n - row - 1; space++)
         {
-            print f(" ");
+            printf(" ");
         }
+
+        // Print hashs base on column
         for (col = 0; col <= row; col++)
         {
             printf("#");
