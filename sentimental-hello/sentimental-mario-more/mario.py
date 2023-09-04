b1f09eabe79c831b62b8ed8eb9485154f24d1ffe
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-04 11:44:45 +0000

    Mon, Sep 4, 2023, 1:44 PM +02:00

--- sentimental-hello/sentimental-mario-more/mario.py
+++ sentimental-hello/sentimental-mario-more/mario.py
@@ -5,14 +5,14 @@ from cs50 import get_int
 while True:
     n = get_int("Enter height here: ")
 
-# Notes as long as height is greater than o we use break
+    # Notes as long as height is greater than o we use break
     if n > 0 and n < 9:
-       break
+        break
 
 # Function that is range to get any number of values and that range actually takes in other functions as well
 for i in range(0, n, 1):
     for j in range(0, n + i + 3, 1):
-        if (j == n or j == n+1 or i + j < n - 1):
+        if j == n or j == n+1 or i + j < n - 1):
 
             # To specfiy that nothing should be printed at the end of our string
             print("", end=" ")
