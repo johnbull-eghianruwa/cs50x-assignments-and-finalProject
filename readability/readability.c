718b8a338351203e7267accfe7c648d14203c8be
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-04 19:40:03 +0000

    Mon, Sep 4, 2023, 9:40 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
@@ -11,6 +12,8 @@ int main(void)
     int letters = 0;
     for (int i = 0; i < strlen(text); i++)
     {
-        if ((text[i] >= "a" && text[i] <= "z"))
+        if ((text[i] >= "a" && text[i] <= "z") || (text[i] >= "A" && text[i] <= "Z"))
+        letters++;
     }
+    printf("%i letters", letters);
 }
