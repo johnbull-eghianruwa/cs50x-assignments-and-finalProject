8c3c9e448f8b989a3b41e58adebf51da80568321
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-04 07:16:03 +0000

    Mon, Sep 4, 2023, 9:16 AM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -36,7 +36,8 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
+    // Open dictionary
+    FILE
     return false;
 }
 
