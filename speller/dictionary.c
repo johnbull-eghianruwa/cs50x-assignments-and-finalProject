b72b67bae604eec9ab17eb2144ec742ffd88108c
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-04 07:46:26 +0000

    Mon, Sep 4, 2023, 9:46 AM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -87,7 +87,7 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
+    // Return size of dictionary
     return 0;
 }
 
