7e2f7efe035bbb1ff28f923b4dae6cd20c9aef21
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-04 19:34:22 +0000

    Mon, Sep 4, 2023, 9:34 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -6,4 +6,11 @@ int main(void)
     // prmpt user for text
     string text = get_string("Type a text here: ");
     printf("%s\n", text);
+
+    // Count the number of letters there are in the text
+    int letters = 0;
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (letters == text)
+    }
 }
