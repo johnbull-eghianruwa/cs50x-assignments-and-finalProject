80debd30cdaafcb54dc0d263be476f5de1f4e8b0
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-04 19:58:17 +0000

    Mon, Sep 4, 2023, 9:58 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,17 +5,7 @@
 
 int main(void)
 {
-    // prmpt user for text
-    string text = get_string("Type a text here: ");
-    printf("%s\n", text);
-
-    // Count the number of letters there are in the text
-    int letters = 0;
-    for (int i = 0; i < strlen(text); i++)
-    {
-        if ((text[i] >= "a" && text[i] <= "z") ||
-        (text[i] >= "A" && text[i] <= "Z"))
-        letters++;
-    }
-    printf("%i letters", letters);
+   // Prompt user for text
+   string text = get_string("<type text here: ");
+   printf("%s\n", text);
 }
