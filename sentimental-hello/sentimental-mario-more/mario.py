d01b686020ed5172d3def37bad4ec32c856775f4
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-04 11:02:02 +0000

    Mon, Sep 4, 2023, 1:02 PM +02:00

--- sentimental-hello/sentimental-mario-more/mario.py
+++ sentimental-hello/sentimental-mario-more/mario.py
@@ -4,6 +4,8 @@ from cs50 import get_int
 # so while true n equal to get into height we will prompt the iser for height
 while True:
 n = get_int("Enter height here: ")
+
+# notes as long as height is greater than o we use break
 if n > 0:
     break
 return height
