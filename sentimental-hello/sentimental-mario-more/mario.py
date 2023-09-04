b0b0fff16086b695bbe5f9f9635ce5f40e167a64
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-04 11:28:02 +0000

    Mon, Sep 4, 2023, 1:28 PM +02:00

--- sentimental-hello/sentimental-mario-more/mario.py
+++ sentimental-hello/sentimental-mario-more/mario.py
@@ -10,7 +10,7 @@ while True:
        break
 for i in range(0, n, 1):
     for j in range(0, n, 1):
-        if (i + j < n-1):
+        if (l == n or j == n+1 or i + j < n):
             print("", end=" ")
         else:
             print("#", end="")
