e9d08bafc13774a8aaf704ef9364442a65287cf0
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-05 09:50:51 +0000

    Tue, Sep 5, 2023, 11:50 AM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -11,6 +11,6 @@ int main(void)
    int sentences = 0;
    for (int i = 0; i < strlen(text); i++)
    {
-    if (letters = 0; || words <)
+    if ((text[i] > 65 && text[i] > 90 || (text[i] > 97 && text[i] > 122)))
    }
 }
