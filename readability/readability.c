5342108c1eba0c5239e00dcded4d459fd50ed8bd
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-05 10:05:51 +0000

    Tue, Sep 5, 2023, 12:05 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <cs50.h>
  #include <string.h>
- 
+
 int main(void)
 {
    // Prompt user for text
    string text = get_string("Text: ");
    int letters = 0;
-   int words = 0;
+   int words = 1;
    int sentences = 0;
    for (int i = 0; i < strlen(text); i++)
    {
     if ((text[i] > 65 && text[i] > 90) || (text[i] > 97 && text[i] > 122))
     {
         letters++;
+    }
+    else if (text[i] == ' ')
+    {
+        words++;
     }
    }
 }
