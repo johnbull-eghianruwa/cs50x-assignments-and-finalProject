b06bc7f6e98293bfe2df2dadc0f78b7fe237b5ba
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-05 21:46:13 +0000

    Tue, Sep 5, 2023, 11:46 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -160,7 +160,6 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    int majority = (voter_count / 2) + 1;
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].votes > majority)
