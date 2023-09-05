2a5e6dc3157b3ace2abb52e88224e6ffe04e0a11
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-05 09:45:51 +0000

    Tue, Sep 5, 2023, 11:45 AM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -6,4 +6,11 @@ int main(void)
 {
    // Prompt user for text
    string text = get_string("Text: ");
+   int letters = 0;
+   int words = 0;
+   int sentences = 0;
+   for (int i = 0; i < strlen(text); i++)
+   {
+    if (letters = 0; || words <)
+   }
 }
