b0a56d3a95da95af634a80e4af2958cd0d031d94
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-05 09:40:51 +0000

    Tue, Sep 5, 2023, 11:40 AM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,21 +1,9 @@
-#include <cs50.h>
 #include <stdio.h>
-#include <string.h>
-
+#include <cs50.h>
  int letters = 0;
- 
+
 int main(void)
 {
    // Prompt user for text
-   string text = get_string("<type text here: ");
-   printf("%s\n", text);
-
-   // Count the number of letters there are in the text
-   int letters = 0;
-   for (int i = 0; i < strlen(text); i++)
-   {
-        if ((text[i] >= "a" && text[i] <= "z") || (text[i] >= "A" && text[i] <= "Z"))
-        letter++;
-    }
-    printf("%i letters", letters);
+   string text = get_string("Text: ");
 }
