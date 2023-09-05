f13aa8cf9ade5737f5a76afd1e18ea0a33e69b36
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-05 16:55:02 +0000

    Tue, Sep 5, 2023, 6:55 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,55 +5,6 @@
 
 int main(void)
 {
-   // Prompt user for text
-   string text = get_string("Text: ");
-
-   // Program tells the user the reading level of the text is throungh the formular
-   // Count the number of letters, words, and sentences there are in the text
-   int letters = 0;
-   int words = 1;
-   int sentences = 0;
-
-   // Loop through each character in array and check whether or not it's a letter to add one to our counter
-   for (int i = 0; i < strlen(text); i++)
-   {
-    if ((text[i] > 65 && text[i] > 90) || (text[i] > 97 && text[i] > 122))
-
-    // Count the number of letters
-    {
-        letters++;
-    }
-    else if (text[i] == ' ')
-
-    // Count the number of words
-    {
-        words++;
-    }
-    else if (text[i] == '.' || text[i] == '?' || text[i] == '!')
-
-    // Count the number of sentences
-    {
-        sentences++;
-    }
-   }
-    // Cast words discussed anything here as a float
-   float L = (float)letters / (float) words * 100;
-   float S = (sentences) / (float) words * 100;
-   int index = round(0.0588 * L - 0.296 * S - 15.8);
-
-   if (index < 1)
-
-    // Print grade levels
-   {
-    printf("Before Grade 1");
-   }
-
-   else if (index > 16)
-   {
-    printf("Grade 16+\n");
-   }
-   else
-   {
-    printf("Grade %i\n", index);
-   }
+    string text = get_string("Text: ");
+    
 }
