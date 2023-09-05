9f886dd5b58338e9d716d9bb1ad6f7ec7689cdfa
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-05 17:28:48 +0000

    Tue, Sep 5, 2023, 7:28 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -26,10 +26,10 @@ int main(void)
         }
     }
     float L = (float) letters / (float) words * 100;
-    float S = (float)semtences / (float)words * 100;
+    float S = (float)sentences / (float)words * 100;
     int index = round (0.0588 * L - 0.296 * S - 15.8);
 
-    if (intex < 1)
+    if (index < 1)
     {
         printf("Before Grade 1");
     }
