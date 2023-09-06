41d06801aec5a93518bc4422cc7715e54e816afe
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-06 18:43:23 +0000

    Wed, Sep 6, 2023, 8:43 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -26,6 +26,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Comb through each row
+    for (int i = 0; i < height; i++)
+    {
+        // Comb through each column
+        for (int j = 0; j < width / 2; j++)
+        {
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - (j + 1)];
+            image[i][width - (j + 1)] = temp;
+        }
+    }
     return;
 }
 
