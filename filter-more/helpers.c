9f992e3767f79897a86a4235868faa634c2a7744
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-06 12:50:04 +0000

    Wed, Sep 6, 2023, 2:50 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -33,7 +33,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         {
             RGBTRIPLE temp = image[i][j];
             image[i][j] = image[i][width - (j + 1)];
-            image[i][width - (j + 1) = temp];
+            image[i][width - (j + 1)] = temp;
         }
     }
     return;
