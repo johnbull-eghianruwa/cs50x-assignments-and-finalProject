dffaab767cff485aade4393f6f27198d3f54f9fd
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-06 11:13:44 +0000

    Wed, Sep 6, 2023, 1:13 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -5,7 +5,7 @@
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     // Comb through each row
-    for (int i =  0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
         // Comb Through each column
         for (int j = 0; j < width; j++)
@@ -29,37 +29,37 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     // Comb through each row
     for (int i = 0; i < height; i++)
     {
-    // Comb through each column
-    for (int j = 0; j < width; j++)
-    {
-        // Convert pixels to float
-        float originalRed = image[i][j].rgbtRed;
-        float originalGreen = image[i][j].rgbtGreen;
-        float originalBlue = image[i][j].rgbtBlue;
+        // Comb through each column
+        for (int j = 0; j < width; j++)
+        {
+            // Convert pixels to float
+            float originalRed = image[i][j].rgbtRed;
+            float originalGreen = image[i][j].rgbtGreen;
+            float originalBlue = image[i][j].rgbtBlue;
 
-        // Find the updated pixel value
-        int sepiaRed = round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
-        int sepiaGreen = round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
-        int sepiaBlue = round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
+            // Find the updated pixel value
+            int sepiaRed = round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
+            int sepiaGreen = round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
+            int sepiaBlue = round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
 
-        // Update the pixel value if sepiaRed, sepiaGreen or sepiaBlue exceeds 255
-        if (sepiaRed > 255)
-        {
-            sepiaRed = 255;
+            // Update the pixel value if sepiaRed, sepiaGreen or sepiaBlue exceeds 255
+            if (sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            if (sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+            if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+            // Update final pixel values
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
         }
-        if (sepiaGreen > 255)
-        {
-            sepiaGreen = 255;
-        }
-        if (sepiaBlue > 255)
-        {
-            sepiaBlue = 255;
-        }
-        // Update final pixel values
-        image[i][j].rgbtRed = sepiaRed;
-        image[i][j].rgbtGreen = sepiaGreen;
-        image[i][j].rgbtBlue = sepiaBlue;
-    }
     }
     return;
 }
@@ -107,7 +107,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 for (int y = -1; y < 2; y++)
                 {
                     int currentX = i + y;
-                    int currentY = j +y;
+                    int currentY = j + y;
 
                     // Check if neighbouring pixel is valid
                     if (currentX < 0 || currentX > (height - 1) || currentY < 0 || currentY > (width - 1))
