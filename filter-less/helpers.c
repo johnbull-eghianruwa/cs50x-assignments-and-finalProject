03ad96b922c7b97a7a0b5420cf9bd991b685125b
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-06 09:46:54 +0000

    Wed, Sep 6, 2023, 11:46 AM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -26,6 +26,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Comb through each row
+    for (int i = 0; i < height; i++)
+    {
     // Comb through each column
     for (int j = 0; j < width; +jj)
     {
@@ -38,6 +41,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         int sepiaRed = rund(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
         int sepiaGreen = round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
         int sepiaBlue = round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
+    }
     }
     return;
 }
