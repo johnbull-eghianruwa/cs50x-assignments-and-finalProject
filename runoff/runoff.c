00865954f5778cefc4b384a4b0bf57977c5ca3bd
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-06 07:34:41 +0000

    Wed, Sep 6, 2023, 9:34 AM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -185,10 +185,10 @@ int find_min(void)
     {
         if (!candidates[i].eliminated && candidates[i].votes < min)
         {
-            min = canddidate
+            min = canddidates[i].votes;
         }
     }
-    return 0;
+    return min;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
