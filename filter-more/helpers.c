ee98aaf6b04a8eb45f815b469feb468c41d88827
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-06 12:41:44 +0000

    Wed, Sep 6, 2023, 2:41 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -17,8 +17,6 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             // Find the average value
             int average = round((Red + Green + Blue) / 3);
             image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = average;
-
-
         }
     }
     return;
