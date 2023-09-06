78dd0079ee83401376c0608cab517a6fd5cf05e4
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-06 11:23:25 +0000

    Wed, Sep 6, 2023, 1:23 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -129,7 +129,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             }
         }
     }
-    // Cpy new pixels into original image
+    // Copy new pixels into original image
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
