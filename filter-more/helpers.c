012f3eec565120d0318f528281b316250d05da73
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-06 18:18:18 +0000

    Wed, Sep 6, 2023, 8:18 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -3,6 +3,18 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Comb through each row
+    for (int i = 0; i < height; i++)
+    {
+        // Comb through each column
+        for (int j = 0; j < width; j++)
+        {
+            // Convert pixels to float
+            float Red = image[i][j].rgbtRed;
+            float Green = image[i][j].rgbtGreen;
+            float <blue = image[i][j].rgbtBlue;
+        }
+    }
     return;
 }
 
