01c25642605e8ccc3cd9a0c5e3098dd9112ef8c1
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-07 07:15:31 +0000

    Thu, Sep 7, 2023, 9:15 AM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -22,6 +22,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+      // sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
+      // sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
+     //sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
+     // As a result, we can guarantee that the resulting red, green, and blue values will be whole number
+     for (int i = 0; i < height; i++)
+     {
+        for (int j = 0; j < width; j++)
+        {
+            i
+        }
+     }
     return;
 }
 
