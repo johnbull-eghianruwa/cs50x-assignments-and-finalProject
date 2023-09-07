c1a59dbd8538649428b331a2e923111ffc56f7b0
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-07 07:02:26 +0000

    Thu, Sep 7, 2023, 9:02 AM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,6 +3,15 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // So convert a pixel to grascale, we just need to make sure
+    // the red, green, amd blue values are all the same value.
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int rgbGray = round ((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3)
+        }
+    }
     return;
 }
 
