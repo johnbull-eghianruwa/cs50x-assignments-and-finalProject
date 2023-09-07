0833ee31ea67425ddc3f5bcce5e77818231d7873
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-07 08:11:51 +0000

    Thu, Sep 7, 2023, 10:11 AM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -51,9 +51,9 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width / 2; j++)
         {
-            temp = image[i][j];
-            image[i][j] = image[i][width - i - 1];
-            image[i][width - j - 1] = temp;
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - (j + 1)];
+            image[i][width - (j + 1)] = temp;
         }
     }
     return;
