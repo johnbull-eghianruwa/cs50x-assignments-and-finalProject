14f0f95f8cd43e30a5a1d37b440bb984420e3e5e
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-11 06:47:27 +0000

    Mon, Sep 11, 2023, 8:47 AM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,1 +1,20 @@
-# TODO
+# Import cs50 get_string library
+from cs50 import get_int
+
+# so while true n equal to get into height we will prompt the iser for height
+while True:
+    n = get_int("Enter height here: ")
+
+    # Notes as long as height is greater than o we use break
+    if n > 0 and n < 9:
+        break
+
+# Function that is range to get any number of values and that range actually takes in other functions as well
+for i in range(0, n, 1):
+    for j in range(0, n + i + 3, 1):
+        if j == n or j == n + 1 or i + j < n - 1:
+            # To specfiy that nothing should be printed at the end of our string
+            print("", end=" ")
+        else:
+            print("#", end="")
+    print()
