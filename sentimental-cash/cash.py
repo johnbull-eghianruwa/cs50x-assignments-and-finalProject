d754efd8e73b3c35ea02c1bbc2b8735423974a77
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-11 14:43:16 +0000

    Mon, Sep 11, 2023, 4:43 PM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -2,11 +2,15 @@
 from cs50 import get_float
 
 while True:
-    n = get_float("Change: ")
+    cents = get_float("Change: ")
     if n > 0:
         break
 
 cents = round(cents*100)
 
 count = 0
+
+# Number of quarters
+while cents >= 25:
+    cents = cens
 
