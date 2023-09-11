273f1c39110449b0100f531c67c097f7ef8eca73
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-11 07:32:47 +0000

    Mon, Sep 11, 2023, 9:32 AM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -7,7 +7,7 @@ while True:
     if number > "American Express" and number < "American Express" and number > "MasterCard" and number < "MasterCard" or number > "Visa card" or number < "Visa card":
 
         for i in range(15):
-            if "Amrican Express" == number
+            if "Amrican Express" == number and "MasterCard" == number or "Visa card" == number:
 
 
     print(f"%i\n", (number))
