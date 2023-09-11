ae21b1e2fbc2258c4dbeaca1554267d7658c766d
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-11 09:42:47 +0000

    Mon, Sep 11, 2023, 11:42 AM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,9 +3,9 @@ from cs50 import get_int
 
 def main():
     while True:
-        credit_card = get_int("Number: ")
-        if credit_card >= 0:
-            break # we look for the valid case here and break out
-
-        if check_validity(credit_card):
-            print
+        card = get_int("Card: ")
+        if card > 0:
+            break
+def luhn_checksum(card):
+    def digits_of(n):
+        return[int(d) for d in str(n)]
