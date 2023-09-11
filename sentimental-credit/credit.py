f52bb6057f88e59ce211129c21c9b0d6c6f405d0
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-11 08:23:31 +0000

    Mon, Sep 11, 2023, 10:23 AM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,6 +4,9 @@ from cs50 import get_int
 while True:
     card = get_int("Enter Card: ")
     if card > 0:
-        break 
+        break
+
+    # Find digits to be multipled by 2
+    card1 = int(card % 10**2 /10)
 
 
