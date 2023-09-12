9bd30977060cb24967124caeee5eba637d338151
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-12 17:23:39 +0000

    Tue, Sep 12, 2023, 7:23 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -6,8 +6,7 @@ typedef uint8_t BYTE;
 int main(int argc, char *argv[])
 {
     // Check that the argument count is 2
-    printf("%i\n", argc);
-    if (argc == 0)
+    if (argc != 2)
     {
         printf("Usage: ./recover IMAGE\n");
         return 1;
