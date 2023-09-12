854aac782d01bac7cafa91ac663c2c8293690b12
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-12 07:20:12 +0000

    Tue, Sep 12, 2023, 9:20 AM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -18,7 +18,7 @@ for i in text:
     elif i == "." or i == "!" or i == "?":
         sentences += 1
 # Coleman-Liau Index
-index = 0.0588 * (letters/words*100) - 0.296 * (sentences/words*100) - 15.8
+index = 0.0588 * (letters / words * 100) - 0.296 * (sentences / words * 100) - 15.8
 
 if index < 1:
     print("Before Grade 1")
