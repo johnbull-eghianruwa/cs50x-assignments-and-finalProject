90fd92365705466906b94350165f995004edc2e9
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-12 09:55:59 +0000

    Tue, Sep 12, 2023, 11:55 AM +02:00

--- dna/dna.py
+++ dna/dna.py
@@ -10,7 +10,7 @@ def main():
     # TODO: Read database file into a variable
     database = []
     with open(sys.argv[1], "r") as file:
-        reader = csv.dictReader(file)
+        reader = csv.DictReader(file)
         for row in reader:
             database.append(row)
 
