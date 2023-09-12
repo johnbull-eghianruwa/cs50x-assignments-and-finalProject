3f4108e68ab747c612ca51f74c1fba1554146530
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-12 07:02:00 +0000

    Tue, Sep 12, 2023, 9:02 AM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -2,6 +2,19 @@
 
 from cs50 import get_string
 
-while True
-    n = get_string("Tex: ")
-    
+# Prompt user for text
+text = get_string("Text: ")
+
+letters = 0
+words = 1
+sentences = 1
+
+# Count number of letters, words and sentences
+for i in text:
+    if i.isalpha():
+        letters += 1
+    elif i == "":
+        words += 1
+    elif i == "." or i == "!" or i == "?":
+        sentences += 1
+        
