01e2fd30d37c8d89926a8d2a513a3b1bd83311d1
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-12 10:02:37 +0000

    Tue, Sep 12, 2023, 12:02 PM +02:00

--- dna/dna.py
+++ dna/dna.py
@@ -15,7 +15,7 @@ def main():
             database.append(row)
 
     # TODO: Read DNA sequence file into a variable
-    with open(sys.argv[2], "r") as file:
+    with open(sys.argv[2], 'r') as file:
         dna_sequence = file.read()
 
     # TODO: Find longest match of each STR in DNA sequence
