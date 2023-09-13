1b9d7bc72dc79924f28dd1d3dd8ab36aa3b9695f
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-13 15:17:06 +0000

    Wed, Sep 13, 2023, 5:17 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 SELECT description FROM crime_scene_reports
-WHERE year - 2021 and month - 7 and - 28;
+WHERE year=2021 and month=7 and=28;
