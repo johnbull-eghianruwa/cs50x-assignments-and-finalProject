84ccc4f3a92c1eea5025e2cebbd2db6dbac92a66
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-14 21:09:47 +0000

    Thu, Sep 14, 2023, 11:09 PM +02:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,3 +1,4 @@
+-- list the names of all people who starred in a movie released in 2004, ordered by birth year
 SELECT DISTINCT name FROM people
 JOIN stars ON stars.person_id = people.id
 JOIN movies ON movies.id = stars.movie_id
