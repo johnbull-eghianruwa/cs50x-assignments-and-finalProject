ed488dd3d9025770d1382d543edc6f63584b3b57
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-14 20:31:50 +0000

    Thu, Sep 14, 2023, 10:31 PM +02:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,2 +1,6 @@
-SELECT all(movie) FROM movies
-WHERE year = 2012 AND rating DESC;
+-- list all movies released in 2010 and their ratings, in descending order by rating.
+-- For movies with the same rating, order them alphabetically by title
+SELECT title, rating FROM movies
+JOIN  ratings ON movies.id = ratings.movies_id
+WHERE year = 2010
+ORDER BY rating DESC, title ASC;
