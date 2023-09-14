3a1fde88deb4c9d04f0527cf01b3a04cff271fda
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-14 21:47:49 +0000

    Thu, Sep 14, 2023, 11:47 PM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT name FROM people
 JOIN stars ON stars.person_id = people.id
-JOIN stars. ON stars.person_id = people.id
+JOIN movies ON movies.id = stars.movie_id
 JOIN movies ON movies.id = stars.movie_id
 WHERE movies.id IN
 (SELECT movies.id FROM movies
