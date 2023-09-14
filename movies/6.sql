56c315df0f4f42b0d70a7ac3eb4ce0a5d57c5dab
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-14 20:15:45 +0000

    Thu, Sep 14, 2023, 10:15 PM +02:00

--- movies/6.sql
+++ movies/6.sql
@@ -1,3 +1,4 @@
 -- query to determine the average rating of all movies released in 2012
-SELECT rating FROM movies
-WHERE 2012 COUNT(1)
+SELECT AVG(rating) FROM movies
+join ratings on movies_id = ratings.movies_id
+WHERE year = 2012;
