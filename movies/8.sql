b8e5fbadae13a0c8add4be8c9bc1d6c971469bc6
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-15 07:11:49 +0000

    Fri, Sep 15, 2023, 9:11 AM +02:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,5 +1,5 @@
 --  list the names of all people who starred in Toy Story
 SELECT name FROM people
 JOIN stars ON stars.person_id = people.id
-JOIN movies ON movies.id = stars.movies_id
+JOIN movies ON movies.id = stars.movie_id
 WHERE movies.title = "Toy Story";
