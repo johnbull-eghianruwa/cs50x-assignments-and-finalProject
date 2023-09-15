eca2d6f8d7bc870c305177f3d92464743705b018
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-15 20:13:07 +0000

    Fri, Sep 15, 2023, 10:13 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -13,4 +13,4 @@ AND backery_security_logs.month = 7
 AND bakery_security_logs.day = 28
 AND bakery_security.logs.hour = 10
 AND bakery_security_logs.minute >= 15
-AND bakery_securi
+AND bakery_security_logs <= 25;
