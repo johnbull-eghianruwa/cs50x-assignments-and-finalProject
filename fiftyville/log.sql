f6a69dbead1e1053876e70a80f5f13ce7264999b
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-15 08:50:08 +0000

    Fri, Sep 15, 2023, 10:50 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -16,11 +16,6 @@ AND bakery_security_logs.hour = 10
 AND bakery_security_logs.minute >= 15
 AND bakery_security_logs.minute <= 25;
 
-SELECT people.name, atm_transactions.transaction_type FROM people
-JOIN bank_acounts ON bank_counts.person_id = people.id
-JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
-WHERE atm_transactions.year = 2021
-AND atm_transactions.month = 7
-AND atm_transactions.day = 28
-AND atm_location = "Leggett Street"
-AND atm_transactions.transaction_type = "withdraw";
+SELECT peope.name, atm_transactions.transaction_type FROM people
+JOIN bank_accounts ON bank_accounts.person_id = people.id
+JOIN atm_
