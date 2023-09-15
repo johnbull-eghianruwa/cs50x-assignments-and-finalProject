fc9ab89904a47b19bdcdee0f7550d7f8f68e75fa
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-15 20:16:34 +0000

    Fri, Sep 15, 2023, 10:16 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -11,6 +11,6 @@ JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license
 WHERE bakery_security_logs.year = 2021
 AND bakery_security_logs.month = 7
 AND bakery_security_logs.day = 28
-AND bakery_security.logs.hour = 10
+AND bakery_security_logs.hour = 10
 AND bakery_security_logs.minute >= 15
 AND bakery_security_logs <= 25;
