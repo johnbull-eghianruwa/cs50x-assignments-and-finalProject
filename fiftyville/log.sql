89c91342b17916e10cb7b702b6813c78c8578e8d
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-15 08:03:57 +0000

    Fri, Sep 15, 2023, 10:03 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -2,6 +2,6 @@
 SELECT description FROM crime_scene_reports
 WHERE year = 2021 AND month = 7 AND day = 28;
 
-SELECT transcript FROM interview
+SELECT transcript FROM interviews
 WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE
-"%Bakery%";
+"%bakery%";
