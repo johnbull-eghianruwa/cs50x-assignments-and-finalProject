c3bf9bea91efdad2bd51765755d3117a845a1286
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-15 07:13:28 +0000

    Fri, Sep 15, 2023, 9:13 AM +02:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,3 +1,4 @@
+-- List the names of all people who have directed a movie that received a rating of at least 9.0
 SELECT DISTINCT name FROM people
 JOIN directors ON directors.person_id = people.id
 JOIN movies ON movies.id = directors.movie_id
