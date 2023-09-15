a0e274d8786e530d60d6c73c819856ee0bbc1f21
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-15 07:16:39 +0000

    Fri, Sep 15, 2023, 9:16 AM +02:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,3 +1,4 @@
+-- List the titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting with the highest rated
 SELECT title FROM movies
 JOIN ratings ON ratings.movie_id = movies.id
 JOIN stars ON stars.movie_id = movies.id
--- movies/12.sql
+++ movies/12.sql
@@ -1,3 +1,4 @@
+-- List the titles of all movies in which both Bradley Cooper and Jennifer Lawrence starred
 SELECT title FROM movies
 JOIN stars ON stars.movie_id = movies.id
 JOIN people ON people.id = stars.person_id
