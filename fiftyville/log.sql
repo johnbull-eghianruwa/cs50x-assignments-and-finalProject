9a14020c7808a24b4e5a73505162f82ddff2054e
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-15 20:15:21 +0000

    Fri, Sep 15, 2023, 10:15 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,7 +9,7 @@ WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
 SELECT bakery_security_logs.activity, bakery_security_logs.license_plate, people.name FROM people
 JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
 WHERE bakery_security_logs.year = 2021
-AND backery_security_logs.month = 7
+AND bakery_security_logs.month = 7
 AND bakery_security_logs.day = 28
 AND bakery_security.logs.hour = 10
 AND bakery_security_logs.minute >= 15
