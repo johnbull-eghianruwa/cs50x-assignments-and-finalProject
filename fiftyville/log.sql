b2305b2a37f0e0507ed3fb95a71a85e9bb490ec3
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-15 16:36:10 +0000

    Fri, Sep 15, 2023, 6:36 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -15,7 +15,7 @@ AND bakery_security_logs.hour = 10
 AND bakery_security_logs.minute >= 15
 AND bakery_security_logs.minute <= 25;
 
-SELECT  bank_accounts.person_id, people.name FROM bank_accounts
+SELECT 
 JOIN bank_accounts ON bank_accounts.person_id = people.id
 JOIN atm_transactions ON atm_transactions.account_number =
 bank_accounts.account_number
