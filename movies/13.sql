3f3f92d326602382ca2b1cc036963b836ff40e03
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-15 07:19:17 +0000

    Fri, Sep 15, 2023, 9:19 AM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,3 +1,4 @@
+-- List the names of all people who starred in a movie in which Kevin Bacon also starred
 SELECT DISTINCT name FROM people
 JOIN stars ON stars.person_id = people.id
 JOIN movies ON movies.id = stars.movie_id
