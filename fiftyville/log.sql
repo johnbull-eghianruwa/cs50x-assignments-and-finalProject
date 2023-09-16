3cadb76c31ee76664291725629c3f98b5572aec8
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-16 09:30:17 +0000

    Sat, Sep 16, 2023, 11:30 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,28 +1,3 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT description FROM crime_scene_reports
 WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street";
-
--- Interviews were conducted today with three witnesses who were present at the time-each of their interview transcripts mentions the backery
-SELECT transcript FROM interviews
-WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
-
--- Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery
-SELECT bakery_security_logs.activity, bakery_security_logs.license_plate, people.name FROM people
-JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
-WHERE bakery_security_logs.year = 2021
-AND bakery_security_logs.month = 7
-AND bakery_security_logs.day = 28
-AND bakery_security_logs.hour = 10
-AND bakery_security_logs.minute >= 15
-AND bakery_security_logs.minute <= 25;
--- Suspects: Vanessa, Bruce, Barry, Luca, Sofia, Iman, Diana, Kelsey
-SELECT name FROM people
-JOIN bank_accounts ON bank_accounts.person_id = people.id
-JOIN atm_transactions ON atm_transactions.account_number = Bank_accounts.account_number
-WHERE year = 2021
-AND month = 7
-AND day = 28
-AND atm_location = "leggett Street"
-AND transaction_type = "withdraw"
-AND (people.name = "Vanessa" OR people.name = "Bruce" OR people.name = "Barry" OR people.name =
-"Luca" OR people.name = "Sofia" OR people.name = "Iman" OR people.name = "Diana" OR people.name = "Kelsey");
