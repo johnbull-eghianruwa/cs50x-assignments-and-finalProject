586916b24152b9cc1159d0ca907662260f1ffc03
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-16 20:12:15 +0000

    Sat, Sep 16, 2023, 10:12 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -49,12 +49,10 @@ LIMIT 1);
 
 -- Phone call
 SELECT city FROM airports
-WHERE id (SELECT destination_airport_id FROM flights
-WHERE year = 2021
-AND month = 7
-AND day = 29
-AND origin _airport_id = (
+WHERE id = (SELECT destination_airport_id FROM flights
+WHERE year = 2021 AND month = 7 AND day = 29 AND origin_airport_id = (
 SELECT id FROM airports WHERE city = "Fiftyville")
 ORDER BY hour,minute
 LIMIT 1);
 --Suspects: Sofia, Kelsey, Bruce, Taylor, Diana, Carina, Kenny, Benista
+
