93105d1fd280e83d42c2f9ccc53f02a21c218d8e
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-16 09:56:16 +0000

    Sat, Sep 16, 2023, 11:56 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,5 +3,5 @@ SELECT description FROM crime_scene_reports
 WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street";
 
 -- Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery. |
-SELECT transcripts FROM interviews
-WHERE year = 2021 AND month = 7 AND day = 28 AND transcripts LIKE "%bakery%";
+SELECT transcript FROM interviews
+WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
