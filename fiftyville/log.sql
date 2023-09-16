4cfd87f921240360bfb9a42d4f3a290af48fccf0
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-16 09:28:45 +0000

    Sat, Sep 16, 2023, 11:28 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,6 +1,6 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT description FROM crime_scene_reports
-WHERE year = 2021 AND month = 7 AND day = 28;
+WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street";
 
 -- Interviews were conducted today with three witnesses who were present at the time-each of their interview transcripts mentions the backery
 SELECT transcript FROM interviews
