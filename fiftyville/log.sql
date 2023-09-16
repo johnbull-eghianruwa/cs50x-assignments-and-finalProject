223a1b8fc1b9676013f952822beb66b4dc251e66
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-16 08:56:33 +0000

    Sat, Sep 16, 2023, 10:56 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -16,7 +16,7 @@ AND bakery_security_logs.hour = 10
 AND bakery_security_logs.minute >= 15
 AND bakery_security_logs.minute <= 25;
 -- Suspects: Vanessa, Bruce, Barry, Luca, Sofia, Iman, Diana, Kelsey
-SELECT bank_accounts.person_id, people.name FROM bank_accounts
+SELECT name FROM people
 JOIN atm_transactions ON atm_transactions.account_number =
 bank_accounts.account_number
 JOIN people ON bank_accounts.person_id = people.id
