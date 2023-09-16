83c8d4e7b2762f8207b6c6096fb50c71f94561b5
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-16 10:02:53 +0000

    Sat, Sep 16, 2023, 12:02 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -7,3 +7,5 @@ SELECT transcript FROM interviews
 WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
 
 -- First transcript!
+-- Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away. If you
+-- have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time -- frame
