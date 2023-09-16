9cfc79689576820e21b5c9daa1a9cea048d7d00e
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-16 19:34:15 +0000

    Sat, Sep 16, 2023, 9:34 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -47,5 +47,9 @@ AND duration < 60;
 --  As the thief was leaving the bakery, they called someone who talked to them for less than a minute.
 -- In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville
 -- tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket.
-
-
+SELECT name FROM people
+JOIN passengers ON passengers.passport_number = people.passport_number
+WHERE passengers.flight_id = (SELECT id FROM flights
+WHERE year = 2021 AND month = 7 AND day = 29 AND origin_airport_id = (
+SELECT id  FROM airports WHERE city = "Fiftyville")
+ORDER BY hour)
