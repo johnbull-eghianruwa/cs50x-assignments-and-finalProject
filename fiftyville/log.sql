fcc6f9c945fb58e1274f1ed46a504e12d4b3596e
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-16 10:18:33 +0000

    Sat, Sep 16, 2023, 12:18 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,14 +9,14 @@ WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
 -- First transcript!
 -- Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away. If you
 -- have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time -- frame
-
+SELECT name FROM people
 JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
 WHERE bakery_security_logs.year = 2021
 AND bakery_security_logs.month = 7
 AND bakery_security_logs.day = 28
 AND bakery_security_logs.hour = 10
 AND bakery_security_logs.minute >= 15
-AND bakery_security_logs.minute <= 25;
+AND bakery_security_logs.minute <= 25
 AND activity = "exit";
 
 
