f68cdbc022b01ab28498e070456ba5778e1ee45c
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-17 07:14:45 +0000

    Sun, Sep 17, 2023, 9:14 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -72,9 +72,4 @@ LIMIT 1);
 
 -- ACCOMPLICE
 SELECT phone_number FROM people WHERE name = "Bruce";
---- sqlite> SELECT city FROM airports
-   ...> WHERE id = (SELECT destination_airport_id FROM flights
-   ...> WHERE year = 2021 AND month = 7 AND day = 29 AND origin_airport_id = (
-   ...> SELECT id FROM airports WHERE city = "Fiftyville")
-   ...> ORDER BY hour,minute
-   ...> LIMIT 1);
+--- (367) 555-5533
