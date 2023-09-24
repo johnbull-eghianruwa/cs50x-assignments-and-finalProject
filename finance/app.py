69b76c7cc0800cc1180c72bde3a2245d0aa6c82f
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-24 13:06:54 +0000

    Sun, Sep 24, 2023, 3:06 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -117,7 +117,13 @@ def register():
         confirmation = request.form.get("confirmation")
 
         if not username:
-            return apology("Must provide username")
+            return apology("Must Provide Username")
+        if not password:
+            return apology("Must Provide Password")
+        if not confirmation:
+            return apology("Must Provide Confirmation")
+        if password != confirmation:
+            return apology("Passwords Do Not Match")
 
 
 @app.route("/sell", methods=["GET", "POST"])
