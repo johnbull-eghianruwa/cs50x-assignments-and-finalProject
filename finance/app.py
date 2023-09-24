b127e980102810b9ee8783dfa3163f01b0200b5b
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-24 21:02:04 +0000

    Sun, Sep 24, 2023, 11:02 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -43,7 +43,13 @@ def index():
 @login_required
 def buy():
     """Buy shares of stock"""
-    return apology("TODO")
+    if request.method == "GET":
+        return render_template("buy.html")
+
+
+    else:
+        symbol = request.form.get("symbol")
+        shares = request.form.get("shares")
 
 
 @app.route("/history")
