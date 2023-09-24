9cc524701c04f57ce97bf79f65ca5746b580aeb6
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-24 20:00:16 +0000

    Sun, Sep 24, 2023, 10:00 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -118,7 +118,7 @@ def quote():
         if stock == None:
             return apology("Symbol Does Not Exists")
 
-        return render_template("quoted.html", name = stock["name"], prince = stock["price"],)
+        return render_template("quoted.html", name = stock["name"], prince = stock["price"], symbol = stock)
 
 
 @app.route("/register", methods=["GET", "POST"])
