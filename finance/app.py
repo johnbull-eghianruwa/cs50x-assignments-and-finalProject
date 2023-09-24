a60caf8616d0cb9fa2ee0708cc7a70992a8aa828
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-24 10:57:21 +0000

    Sun, Sep 24, 2023, 12:57 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -111,6 +111,13 @@ def register():
     """Register user """
     if request.method == "GET":
         return render_template("register.html")
+    else:
+        username = request.form.get("user")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if not username:
+            return apology("Must ")
 
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
