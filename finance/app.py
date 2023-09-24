d78f9bff05e9c0a8819eaeda25c788393c10b482
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-24 13:13:01 +0000

    Sun, Sep 24, 2023, 3:13 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -126,6 +126,10 @@ def register():
         if password != confirmation:
             return apology("Passwords Do Not Match")
 
+        hash = generate_password_hash(password)
+
+        try:
+
 
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
