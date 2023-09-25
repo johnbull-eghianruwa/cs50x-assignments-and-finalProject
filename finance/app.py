5f58e13f7491dc13b4c1f9eb89826e66545099c6
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-25 07:53:34 +0000

    Mon, Sep 25, 2023, 9:53 AM +02:00

--- finance/app.py
+++ finance/app.py
@@ -78,7 +78,7 @@ def buy():
 
          #INSERT INTO table_name (column1, column2, column3, ...) VALUES (value1, value2, value3, ...)
         db.execute("INSERT INTO transactions (user_id, symbol, shares price, date) VALUES (?, ?, ?, ?, ?)", user_id, stock["symbol"], shares, stock["price"], date)
-        
+        flask("Bought!")
 
 
 
