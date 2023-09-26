664de8a9b531ddbc46160543c2a690043400f9df
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-26 20:57:06 +0000

    Tue, Sep 26, 2023, 10:57 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -205,5 +205,5 @@ def sell():
     if request.method == "GET":
         user_id = session["user_id"]
         symbols_user = db.execute("SELECT symbol FROM transaction WHERE user_id = :id HAVING SUM(shares) > 0", id=user_id )
-        return render_template("sell.html")
+        return render_template("sell.html", symbols = )
 
