5760a943b67ab6d44d7b9087f1c52ebf513c6ee4
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-26 21:40:06 +0000

    Tue, Sep 26, 2023, 11:40 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -222,6 +222,7 @@ def sell():
         user_cash = user_cash_db[0]["cash"]
 
         user_shares = db.execute("SELECT cash FROM transactions WHERE id=:id AND symbol = :symbol GROUP BY symbol", user_id, symbol)
+        user_shares_real = user_shares[0]
 
         uptd_cash = user_cash + transaction_value
 
