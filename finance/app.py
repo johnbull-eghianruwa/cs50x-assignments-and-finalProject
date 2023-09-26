d56d01b9e9a74dd6553770b1ee40884bfb7449e1
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-26 20:53:06 +0000

    Tue, Sep 26, 2023, 10:53 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -204,4 +204,4 @@ def sell():
     """Sell shares of stock"""
     if request.method == "GET":
         user_id = session["user_id"]
-        symbol_user = db.execute("SELECT symbol FROM transaction WHERE user_id =")
+        symbol_user = db.execute("SELECT symbol FROM transaction WHERE user_id = :id", )
