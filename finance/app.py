ef96365964b2f5e76541023eec003cf8f23ff027
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-27 07:56:50 +0000

    Wed, Sep 27, 2023, 9:56 AM +02:00

--- finance/app.py
+++ finance/app.py
@@ -96,6 +96,14 @@ def history():
     transactions_db = db.execute("SELECT * FROM transactions WHERE user_id = :id=user_id")
     return render_template("history.html", transactions = transactions_db)
 
+@app.route("/add_cash")
+@login_required
+def add_cash():
+    """User can add cash"""
+    user_id = session["user_id"]
+    transactions_db = db.execute("SELECT * FROM transactions WHERE user_id = :id=user_id")
+    return render_template("history.html", transactions = transactions_db)
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
