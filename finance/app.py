48869133cfc78d7f1c86e800bb92a0c92267d15f
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-29 16:20:27 +0000

    Fri, Sep 29, 2023, 6:20 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -96,6 +96,7 @@ def buy():
 @login_required
 def history():
     """Show history of transactions"""
+    user_id = session["user_id"]
     transactions_db = db.execute("SELECT * FROM transactions WHERE user_id = :id=user_id")
     return render_template("history.html", transactions = transactions_db)
 
