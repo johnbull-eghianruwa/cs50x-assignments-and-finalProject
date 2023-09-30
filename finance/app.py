edb820ccd86a4782ac77e0adc9a318142cf53e42
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-09-30 22:14:51 +0000

    Sun, Oct 1, 2023, 12:14 AM +02:00

--- finance/app.py
+++ finance/app.py
@@ -106,6 +106,11 @@ def add_cash():
     user_id = session["user_id"]
     if request.method ==    "GET":
         return render_template("add.html")
+    else:
+    new_cash = request.form.get("new_cash")
+
+    if not new_cash:
+        return apology("You Must Give Money")
 
 
 @app.route("/login", methods=["GET", "POST"])
