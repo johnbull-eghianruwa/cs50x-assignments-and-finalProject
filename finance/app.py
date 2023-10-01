c74ea8f3a1a6b610cb0e7a52a55b387c64643480
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-10-01 19:17:20 +0000

    Sun, Oct 1, 2023, 9:17 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -257,7 +257,7 @@ def sell():
          user_cash = user_cash_db[0]["cash"]
 
          user_shares = db.execute("SELECT shares FROM transactions WHERE user_id=:id AND symbol = :symbol GROUP BY symbol", id=user_id, symbol= symbol)
-         user_shares_real = user_shares[0]["shares"]
+         user_shares_real = user_shares[0]["cash"]
 
          if shares > user_shares_real:
             return apology("You Do Not Have This Amount Of Shares")
