2f41346bd14d4e8e0b44ba62e358fdcf4ad07415
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-10-02 20:54:03 +0000

    Mon, Oct 2, 2023, 10:54 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -232,46 +232,4 @@ def register():
 @login_required
 def sell():
     """Sell shares of stock"""
-    if request.method == "GET":
-        user_id = session["user_id"]
-        symbols_user = db.execute("SELECT symbol FROM transactions WHERE user_id = :id GROUP BY symbol HAVING SUM(shares) > 0", id=user_id )
-        return render_template("sell.html", symbols = [row["symbol"] for row in symbols_user])
-
-    else:
-         symbol = request.form.get("symbol")
-         shares = int(request.form.get("shares"))
-
-         if not symbol:
-            return apology("Must Give Symbol")
-
-         stock = lookup(symbol.upper())
-         if stock == None:
-            return apology("Symbol Does Not Exist")
-
-         if shares < 0:
-            return apology("Share Not Allowed")
-
-         transaction_value = shares *stock["price"]
-
-         user_id = session["user_id"]
-         user_cash_db = db.execute("SELECT cash FROM users WHERE id = :id", id=user_id,)
-         user_cash = user_cash_db[0]["cash"]
-
-         user_shares =db.execute("SELECT shares AS total_shares FROM users WHERE user_id=:id AND symbol=:symbol GROUP BY symbol", id=user_id, symbol=symbol)
-         user_shares_real = user_shares[0][shares]
-
-         if shares > user_shares_real:
-             return apology("You Do Not Have This Amount Of Shares")
-
-         uptd_cash = user_cash + transaction_value
-
-         # UPDATE table_name SET column1 = value1, column2 = value2, ...WHERE condition
-         db.execute("Update users SET cash = ? WHERE id = ?", uptd_cash, user_id)
-
-         date = datetime.datetime.now()
-
-         #INSERT INTO table_name (column1, column2, column3, ...) VALUES (value1, value2, value3, ...)
-         db.execute("INSERT INTO transactions (user_id, symbol, shares price, date) VALUES (?, ?, ?, ?, ?)", user_id, stock["symbol"], (-1)*shares, stock["price"], date)
-         flash("Sold!")
-
-         return redirect("/")
+   
