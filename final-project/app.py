a1967cdaa298928af1d46f9700efb76afe370f27
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-10-11 11:54:37 +0000

    Wed, Oct 11, 2023, 1:54 PM +02:00

--- final-project/app.py
+++ final-project/app.py
@@ -6,4 +6,17 @@ from flask import flask
 from werkzeug.security import check_password_hash, generate_password_hash
 import datetime
 from sqlalchemy import create_engine
-from helpers import message, login_reqired, lookup, used
+from helpers import apology, login_reqired, lookup, used
+
+app = Flask(_name_)
+
+app.jinja_env.filters["usd"] = usd
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db = SQL("sqlite:///finance.db")
+
+
+
