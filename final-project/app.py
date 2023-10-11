88230020cd8d439e8d45fcf2b1d8c10b57e33551
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-10-11 11:38:23 +0000

    Wed, Oct 11, 2023, 1:38 PM +02:00

--- final-project/app.py
+++ final-project/app.py
@@ -2,4 +2,5 @@ import os
 
 from flask import Flask, flask, redirect, render_template, request, session
 from flask_session import Session
-from flask import
+from flask import flask
+from werkzeug.security import check_password_hash, generate_password_hash
