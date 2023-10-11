70271991dbeedbfbf9471baf02a954ae674db953
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-10-11 11:56:23 +0000

    Wed, Oct 11, 2023, 1:56 PM +02:00

--- final-project/app.py
+++ final-project/app.py
@@ -18,5 +18,38 @@ Session(app)
 
 db = SQL("sqlite:///finance.db")
 
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    """Log user in"""
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POSt ( as by submitting a form via POST)
+    if request.method == "POST":
+        if not request.form.get("Username"):
+            return apology("Username is mandatory", 403)
+
+        elif not request.form.get("Password"):
+            return apology("Password is mandatory", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("incorrect username and/or password",403)
+        session["user_id"] = rows[0]["id"]
+
+        return redirect("/")
+
+    else:
+        return render_template("login.html")
 
 
