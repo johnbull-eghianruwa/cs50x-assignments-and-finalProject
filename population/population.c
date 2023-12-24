3876b906b5455a013b8f74327f63efa9cf61a628
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-12-24 20:49:52 +0000

    Sun, Dec 24, 2023, 9:49 PM +01:00

--- population/population.c
+++ population/population.c
@@ -20,7 +20,7 @@ int main(void)
     while (initialPopulation > targetPopulation);
 
     // TODO: Calculate number of years until we reach threshold
-    int years = 0;
+    int year = 0;
     do
     {
         initialPopulation = initialPopulation + (initialPopulation / 3) - (initialPopulation / 4);
