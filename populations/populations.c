efdae2fc7102f596ec2d88b950d74d382349a66f
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-12-24 20:12:57 +0000

    Sun, Dec 24, 2023, 9:12 PM +01:00

--- populations/populations.c
+++ populations/populations.c
@@ -6,10 +6,16 @@ int main(void)
     // TODO: Prompt for start size
     int start;
     do{
-        start = get_int("Starting population: ");
+        start = get_int("Starting Population: ");
     }
     while(start < 9);
     // TODO: Prompt for end size
+    int end;
+    do
+    {
+        end = get_int("Ending Population: ");
+    }
+    while(start > end);
 
     // TODO: Calculate number of years until we reach threshold
 
