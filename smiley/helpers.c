98d0eb862f2643d66d718218ef6d0b5ea6b5ad20
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-12-24 23:39:31 +0000

    Mon, Dec 25, 2023, 12:39 AM +01:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -3,7 +3,7 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
-    for(int row =0; < width; row++){
+    for(int row = 0; < width; row++){
         for(int col = 0; col < height; col++){
             if(image[col][row].rgbtBlue == 0 &&
             image[col][row].rgbtGreen == 0 &&
