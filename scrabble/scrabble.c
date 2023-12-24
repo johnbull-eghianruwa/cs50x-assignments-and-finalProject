deea51475533d217915a5f68d1bc88e0ab4d71ae
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-12-24 21:15:53 +0000

    Sun, Dec 24, 2023, 10:15 PM +01:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -24,4 +24,11 @@ int main(void)
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
+
+    int score = 0;
+
+    for (int i = 0; i < strlen(word); i++)
+    {
+        
+    }
 }
