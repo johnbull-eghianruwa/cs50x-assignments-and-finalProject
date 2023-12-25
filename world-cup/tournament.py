36b3c092dbb95dc47a0ee31513a67baddf07cc22
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-12-25 09:45:19 +0000

    Mon, Dec 25, 2023, 10:45 AM +01:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,6 +16,12 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
+    filename = sys.argv[1]
+
+    with open(filename) as file:
+        reader = csv.dictReader(file)
+        for team in reader:
+            team["rating"] = int(team["rating])
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
