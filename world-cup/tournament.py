1823d7f93ed602e0c2489bc937e5957e33889d3e
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-12-25 16:12:58 +0000

    Mon, Dec 25, 2023, 5:12 PM +01:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -5,6 +5,7 @@ import random
 # Number of simulations to run
 N = 1000
 
+
 def main():
     # Ensure correct usage
     if len(sys.argv) != 2:
@@ -23,7 +24,7 @@ def main():
     counts = {}
 
     # Simulate N tournaments and keep track of win counts
-    for _ in range(N): 
+    for _ in range(N):
         winner = simulate_tournament(teams)
         if winner in counts:
             counts[winner] += 1
@@ -34,12 +35,14 @@ def main():
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
 
+
 def simulate_game(team1, team2):
     """Simulate a game. Return True if team1 wins, False otherwise."""
     rating1 = team1["rating"]
     rating2 = team2["rating"]
     probability = 1 / (1 + 10 ** ((rating2 - rating1) / 600))
     return random.random() < probability
+
 
 def simulate_round(teams):
     """Simulate a round. Return a list of winning teams."""
@@ -54,11 +57,13 @@ def simulate_round(teams):
 
     return winners
 
+
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     while len(teams) > 1:
         teams = simulate_round(teams)
     return teams[0]["team"]
+
 
 if __name__ == "__main__":
     main()
