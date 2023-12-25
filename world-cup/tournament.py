ad56e231fa6d9f8ce7337434f8a1e1c9e8ca2a58
Johnull Eghianruwa <106601034+johnbull-eghianruwa@users.noreply.github.com>
2023-12-25 10:28:32 +0000

    Mon, Dec 25, 2023, 11:28 AM +01:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -1,32 +1,29 @@
-# Simulate a sports tournament
-
 import csv
 import sys
 import random
 
-# Number of simluations to run
+# Number of simulations to run
 N = 1000
 
-
 def main():
-
     # Ensure correct usage
     if len(sys.argv) != 2:
         sys.exit("Usage: python tournament.py FILENAME")
 
     teams = []
-    # TODO: Read teams into memory from file
-    filename = sys.argv[1]
 
+    # Read teams into memory from file
+    filename = sys.argv[1]
     with open(filename) as file:
-        reader = csv.dictReader(file)
+        reader = csv.DictReader(file)
         for team in reader:
             team["rating"] = int(team["rating"])
             teams.append(team)
 
     counts = {}
-    # TODO: Simulate N tournaments and keep track of win counts
-    for in range(N):
+
+    # Simulate N tournaments and keep track of win counts
+    for _ in range(N): 
         winner = simulate_tournament(teams)
         if winner in counts:
             counts[winner] += 1
@@ -37,14 +34,12 @@ def main():
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
 
-
 def simulate_game(team1, team2):
     """Simulate a game. Return True if team1 wins, False otherwise."""
     rating1 = team1["rating"]
     rating2 = team2["rating"]
     probability = 1 / (1 + 10 ** ((rating2 - rating1) / 600))
     return random.random() < probability
-
 
 def simulate_round(teams):
     """Simulate a round. Return a list of winning teams."""
@@ -59,14 +54,11 @@ def simulate_round(teams):
 
     return winners
 
-
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    # TODO
     while len(teams) > 1:
         teams = simulate_round(teams)
     return teams[0]["team"]
-
 
 if __name__ == "__main__":
     main()
